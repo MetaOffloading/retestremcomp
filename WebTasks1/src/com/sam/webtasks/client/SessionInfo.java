bf3cb7e5eedac25759ac0713b44e767b1ce63a8c
AnnikaBoldt <a.boldt@ucl.ac.uk>
2023-06-05 15:52:31 +0100

    Allow both counterbalancing conditions.

--- WebTasks1/src/com/sam/webtasks/client/SessionInfo.java
+++ WebTasks1/src/com/sam/webtasks/client/SessionInfo.java
@@ -44,7 +44,7 @@ public class SessionInfo {
 	//NB levels range from 0 to (maximum - 1)
 	public static String[] counterbalanceFactors = {"counterbtargetvals"};
 	public static int[] counterbalanceLevels = {2};
-	public static int[] specifiedLevels = {1}; //{-1}; // collect remaining datasets, second counterbalancing group
+	public static int[] specifiedLevels = {-1};
 	
 	/*************************************************/
     /* no need to edit the settings below this point */
